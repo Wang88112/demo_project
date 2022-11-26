21e50124db42c9d3b728d0a9966d817ecb2f551f
user <user@LAPTOP-CPCM6TV9>
2022-11-26 21:12:29 +0800

    4 Commit

--- src/test/java/com/example/demo_project/DemoProjectApplicationTests.java
+++ src/test/java/com/example/demo_project/DemoProjectApplicationTests.java
@@ -1,17 +1,67 @@
 package com.example.demo_project;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.demo_project.entity.Person;
-import com.example.demo_project.repository.PersonDao;
 
 
-@SpringBootTest
+//@SpringBootTest
 class DemoProjectApplicationTests {
+	
+	
+	
+	@Test
+	public void twoSumTest() {
+		int[] intArray = {2,7,11,15};
+//		int target = 9;
+//		for(int x = 0; x < nums.length; x++ ) {
+//			for(int y = x + 1; y < nums.length; y++) {
+//				if(nums[x] + nums[y] == target) {
+//					int[] a = {x, y};
+//				}
+//			}
+//		}
+		
+		int[] result = twoSum(intArray, 9);
+		for(int item : result) {
+			System.out.println(item);
+		}
+	}
+	
+	public int[] twoSum1(int[] nums, int target) {
+		int count = 0;
+		for(int i = 0; i < nums.length - 1; i++) {
+			int num1 = nums[i];
+			for(int j = i + 1; j < nums.length; j++) {
+				count++;
+				int num2 = nums[j];
+				if(num1 + num2 == target) {
+					System.out.println(count);
+					return new int[] {i, j};
+				}
+			}
+	}
+		return null;
+}
+	//================
+	public int[] twoSum(int[] nums, int target) {
+		//<Key, value>
+		Map<Integer, Integer> map = new HashMap<>();
+		int count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			count++;
+			int complement = target - nums[i];
+			if(map.containsKey(complement)) {
+				System.out.println(count);
+				return new int[] {map.get(complement), i};
+		}
+			map.put(nums[i], i);
+	}
+		return null;
+}
 	
 //	@Autowired
 //	private PersonDao personDao;
